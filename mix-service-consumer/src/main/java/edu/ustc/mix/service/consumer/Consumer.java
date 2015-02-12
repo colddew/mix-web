3a98d496feb8fd85847e403ea88cef1cfe410bc1
colddew <88028842@qq.com>
2015-02-12 23:28:58 +0800

    rename project

---
+++ mix-service-consumer/src/main/java/edu/ustc/mix/service/consumer/Consumer.java
@@ -0,0 +1,29 @@
+package edu.ustc.mix.service.consumer;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import edu.ustc.mix.service.DubboAnnotationService;
+import edu.ustc.mix.service.DubboService;
+
+public class Consumer {
+	
+	public static void main(String[] args) throws Exception {
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				new String[] {"applicationContext-dubbo.xml"});
+		context.start();
+		
+		// 获取远程服务代理
+		DubboService dubboService = (DubboService) context.getBean("dubboService");
+		// 执行远程方法
+		String service = dubboService.invoke("dubbo");
+		System.out.println(service);
+		
+		DubboAnnotationService dubboAnnotationService = (DubboAnnotationService) context.getBean("dubboAnnotationService");
+		String annotationService = dubboAnnotationService.invoke("dubbo annotation");
+		System.out.println(annotationService);
+		
+		// 按任意键退出
+		System.in.read();
+	}
+}
