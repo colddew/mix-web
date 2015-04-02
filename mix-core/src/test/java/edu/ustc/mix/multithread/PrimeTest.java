f55ea0dc0612d61be51a67fc0733fdf73e87e530
colddew <88028842@qq.com>
2015-04-02 23:44:14 +0800

    add join test

--- mix-core/src/test/java/edu/ustc/mix/multithread/PrimeTest.java
+++ mix-core/src/test/java/edu/ustc/mix/multithread/PrimeTest.java
@@ -11,4 +11,17 @@ public class PrimeTest {
 			new Thread(new PrimeRun(i)).start();
 		}
 	}
+	
+	@Test
+	public void testJoin() throws Exception {
+		
+		for(int i = 0; i < 50; i++) {
+			PrimeThread primeThread = new PrimeThread(i);
+			primeThread.start();
+			primeThread.join();
+		}
+		
+		Thread thread = new Thread(new PrimeRun(999));
+		thread.start();
+	}
 }
